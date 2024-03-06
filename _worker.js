f2be7aa6478fa8c731d3b4c9844819cf89f661fc
Kingptraaua <162402753+Kingptraaua@users.noreply.github.com>
2024-03-06 19:29:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '3eb981c5-56d9-47e4-8e6e-35fc4ee899b0';
 
-const proxyIPs = ['8.222.214.13'];
+const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
